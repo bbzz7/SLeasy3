b4ed173e5b54e7019c6b9ec60a2cb5a4bdb44e2d
ZhuangYu <bbzz7@163.com>
2016-05-24 15:33:18 +0800

    添加完善了所有jssdk接口检测列表

--- plugin/jssdk.js
+++ plugin/jssdk.js
@@ -1,19 +1,11 @@
-// JavaScript Document
 //jssdk for wechat 1.0.0 by 庄宇 2015-01-25 email:30755405
 ;(function (jssdk, $) {
-    //var time=new Date().getTime();
-    //console.log(time);
-    //var url='http://115.29.102.189/project/youa/index.php/jssdk';//签名后端
 
     //check weChat
     jssdk.isWechat = jssdk.isWeixin = function () {
         var ua = window.navigator.userAgent.toLowerCase();
-        if (ua.match(/MicroMessenger/i) == 'micromessenger') {
-            return true;
-        } else {
-            return false;
-        }
-    }
+        return (ua.match(/MicroMessenger/i) == 'micromessenger');
+    };
 
     //初始化
     jssdk.init = function (myOpt) {
@@ -24,7 +16,7 @@
             time: new Date().getTime(),
             signUrl: 'http://h5.tsnc98.com/CI/index.php/jssdk'//签名后端
             //signUrl:'http://zhuzhouzhixin.com/CI/index.php/jssdk'//签名后端
-        }
+        };
         $.extend($config, myOpt);
         if (jssdk.isWechat() || $config.debug) {
             $.get($config.signUrl, $config, function (json) {
@@ -43,40 +35,41 @@
             }, 'jsonp');
         }
         return dfd.promise();
-    }
+    };
 
 
     //接口检测
     jssdk.check = function () {
         wx.checkJsApi({
             jsApiList: [
-                'checkJsApi',
                 'onMenuShareTimeline',
                 'onMenuShareAppMessage',
                 'onMenuShareQQ',
                 'onMenuShareWeibo',
-                'hideMenuItems',
-                'showMenuItems',
-                'hideAllNonBaseMenuItem',
-                'showAllNonBaseMenuItem',
-                'translateVoice',
+                'onMenuShareQZone',
                 'startRecord',
                 'stopRecord',
-                'onRecordEnd',
+                'onVoiceRecordEnd',
                 'playVoice',
                 'pauseVoice',
                 'stopVoice',
+                'onVoicePlayEnd',
                 'uploadVoice',
                 'downloadVoice',
                 'chooseImage',
                 'previewImage',
                 'uploadImage',
                 'downloadImage',
+                'translateVoice',
                 'getNetworkType',
                 'openLocation',
                 'getLocation',
                 'hideOptionMenu',
                 'showOptionMenu',
+                'hideMenuItems',
+                'showMenuItems',
+                'hideAllNonBaseMenuItem',
+                'showAllNonBaseMenuItem',
                 'closeWindow',
                 'scanQRCode',
                 'chooseWXPay',
@@ -92,10 +85,10 @@
                 alert("检测失败：" + JSON.stringify(res));
             },
             complete: function (res) {
-                alert("检测结束");
+                alert("检测结束:" + JSON.stringify(res));
             }
         });
-    }
+    };
 
 
     //分享
@@ -182,11 +175,11 @@
             }
         });
 
-    }
+    };
 
     //选择图片
     jssdk.chooseImage = function (opt) {
-        var dfd=$.Deferred();
+        var dfd = $.Deferred();
         var $config = {
             count: 1, // 默认9
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -196,17 +189,16 @@
                 var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                 dfd.resolve(localIds);
             }
-        }
+        };
         $.extend($config, opt);
         wx.chooseImage($config);
         return dfd.promise();
-    }
-
+    };
 
 
     //上传图片
-    jssdk._uploadImage=function (opt) {
-        var dfd=$.Deferred();
+    jssdk._uploadImage = function (opt) {
+        var dfd = $.Deferred();
         var $config = {
             localId: opt.localId, // 需要上传的图片的本地ID，由chooseImage接口获得
             isShowProgressTips: 1, // 默认为1，显示进度提示
@@ -215,19 +207,19 @@
                 var serverId = res.serverId; // 返回图片的服务器端ID
                 dfd.resolve(serverId);
             }
-        }
+        };
         $.extend($config, opt);
         wx.uploadImage($config);
         return dfd.promise();
-    }
+    };
 
 
     //上传单张图片,并下载到本地服务器
     jssdk.uploadImage = function (opt) {
         var $config = {
             localId: '', // 需要上传的图片的本地ID，由chooseImage接口获得
-            apiUrl: 'http://www.zhuzhouzhixin.com/CI/index.php/weChatPic/',//
-        }
+            apiUrl: 'http://www.zhuzhouzhixin.com/CI/index.php/weChatPic/'//图片上传地址
+        };
         $.extend($config, opt);
         return jssdk._uploadImage($config).then(function (serverId) {
             return $.get($config.apiUrl, {mediaId: serverId}, '', 'jsonp');
