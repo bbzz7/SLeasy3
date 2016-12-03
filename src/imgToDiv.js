2c94d48a03f3ee8693463ddedb00aa7abc518010
ZhuangYu <bbzz7@163.com>
2016-12-03 18:14:43 +0800

    修正img load事件为one('load')，防止动态改变img src的时候，再次执行load回调

--- src/imgToDiv.js
+++ src/imgToDiv.js
@@ -1,15 +1,15 @@
 // SLeasy3-imgToDiv
 ;(function (SLeasy, $) {
     var $config = SLeasy.config(),
-        $scope  = SLeasy.scope();
+        $scope = SLeasy.scope();
 
     SLeasy.imgToDiv = function ($myDom) {
         var $dom = $myDom || $scope.sliderBox;
         //to div
         $dom.find(".toDiv img").each(function (index, element) {//获取所有图片宽度
             $(this).load(function (e) {
-                var w     = $(this)[0].width,
-                    h     = $(this)[0].height,
+                var w = $(this)[0].width,
+                    h = $(this)[0].height,
                     style = {
                         'backgroundImage': 'url(' + $(this).attr("src") + ')',
                         'backgroundRepeat': 'no-repeat',
@@ -24,13 +24,14 @@
         });
         //no to div
         $dom.find(".noDiv img").each(function (index, element) {//获取所有图片宽度
-            $(this).load(function (e) {
-                var w     = $(this)[0].width,
-                    h     = $(this)[0].height,
+            $(this).one('load', function (e) {
+                var w = $(this)[0].width,
+                    h = $(this)[0].height,
                     style = {
                         'width': w * $scope.viewScale + 'px',
                         'height': h * $scope.viewScale + 'px'
                     }
+                // console.log('============'+w+':'+h+'==============');
                 $(this).css(style);
             });
         });
