8b32d8e116c045bbd6d0e147ece91bf93d71245b
ZhuangYu <bbzz7@163.com>
2016-12-03 18:15:09 +0800

    修正img load事件为one('load')，防止动态改变img src的时候，再次执行load回调

--- build/SLeasy3.js
+++ build/SLeasy3.js
@@ -1743,15 +1743,15 @@ this._dash=b+d,this._offset=b-a[1]+d,this._addTween(this,"_offset",this._offset,
 // SLeasy3-imgToDiv
 ;(function (SLeasy, $) {
     var $config = SLeasy.config(),
-        $scope  = SLeasy.scope();
+        $scope = SLeasy.scope();
 
     SLeasy.imgToDiv = function ($myDom) {
         var $dom = $myDom || $scope.sliderBox;
         //to div
         $dom.find(".toDiv img").each(function (index, element) {//获取所有图片宽度
             $(this).load(function (e) {
-                var w     = $(this)[0].width,
-                    h     = $(this)[0].height,
+                var w = $(this)[0].width,
+                    h = $(this)[0].height,
                     style = {
                         'backgroundImage': 'url(' + $(this).attr("src") + ')',
                         'backgroundRepeat': 'no-repeat',
@@ -1766,13 +1766,14 @@ this._dash=b+d,this._offset=b-a[1]+d,this._addTween(this,"_offset",this._offset,
         });
         //no to div
         $dom.find(".noDiv img").each(function (index, element) {//获取所有图片宽度
-            $(this).load(function (e) {
-                var w     = $(this)[0].width,
-                    h     = $(this)[0].height,
+            $(this).one('load', function (e) {
+                var w = $(this)[0].width,
+                    h = $(this)[0].height,
                     style = {
                         'width': w * $scope.viewScale + 'px',
                         'height': h * $scope.viewScale + 'px'
                     }
+                // console.log('============'+w+':'+h+'==============');
                 $(this).css(style);
             });
         });
