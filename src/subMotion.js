d69aedaf3bf6b543290c85ee4e87c7f1f89db920
bbzz7 <bbzz7@163.com>
2016-12-05 21:49:41 +0800

    添加子动画开始时间绝对定位计算的注释说明

--- src/subMotion.js
+++ src/subMotion.js
@@ -50,13 +50,26 @@
                 preSubMotion = subMotionArr[i - 1],//上一子动画
                 $dom         = $('#SLeasy_' + (subName[type] || type) + '_' + subMotion.index),//当前子动画元素dom
                 time         = subMotion.time || 0,//time
-                preTime      = preSubMotion && preSubMotion.time ? preSubMotion.time : 0;
-            totalTime = preSubMotion ? (totalTime + (subMotion.start ? (preTime - subMotion.start > subMotion.time ? 0 : subMotion.time - (preTime - subMotion.start)) : subMotion.time)) : time,
-                startTime = preSubMotion ? (startTime + (time ? (typeof subMotion.start != 'undefined' ? subMotion.start : preTime) : 0)) : $config.motionTime,
-                // offsetTime   = preSubMotion ? (typeof subMotion.start != 'undefined' ? preSubMotion.time - subMotion.start : 0) : (typeof subMotion.start != 'undefined' ? -subMotion.start : -$config.motionTime),//和上个子动画之间的间隔时间
-                subIn = $.extend({force3D: true}, subMotion.in || {}),//in
-                subShow = $.extend({display: 'block', force3D: true}, subMotion.show || {}),//show
-                set = subMotion.set ? $.extend({position: 'absolute'}, subMotion.set) : {position: 'absolute'};//set
+                preTime      = preSubMotion && preSubMotion.time ? preSubMotion.time : 0,
+                /*
+                 如果是第一个子动画，则当前子动画总时间累加值为，当前子动画时间，
+                 如果不是第一个子动画，则当前子动画总时间累加值为，当前子动画时间:
+                    如果当前子动画有设置start值:
+                        如果preTime - subMotion.start > subMotion.time，累加0
+                        否则累加subMotion.time - (preTime - subMotion.start)
+                    如果当前子动画没有设置start值，则累加上一子动画的运动时间
+                 */
+                totalTime    = preSubMotion ? (totalTime + (subMotion.start ? (preTime - subMotion.start > subMotion.time ? 0 : subMotion.time - (preTime - subMotion.start)) : subMotion.time)) : subMotion.time,
+                /*
+                 如果是第一个子动画，则子动画开始时间为幻灯页运动完成的时间，
+                 如果不是第一个子动画，则之前累加的startTime，加上当前的start值:
+                    如果当前子动画没有设置start值，则累加上一子动画的运动时间，以连接其后
+                    如果当前子动画没有设置运动时间time，则直接加0
+                 */
+                startTime    = preSubMotion ? (startTime + (time ? (typeof subMotion.start != 'undefined' ? subMotion.start : preTime) : 0)) : $config.motionTime,
+                subIn        = $.extend({force3D: true}, subMotion.in || {}),//in
+                subShow      = $.extend({display: 'block', force3D: true}, subMotion.show || {}),//show
+                set          = subMotion.set ? $.extend({position: 'absolute'}, subMotion.set) : {position: 'absolute'};//set
 
             //判断当前幻灯是否包含ae渲染层
             if ($dom.find('.SLeasy_ae').length) {
