f77779271dcb143cf51019177c6a1d38daa1d062
bbzz7 <bbzz7@163.com>
2017-07-15 02:24:05 +0800

    ae内置插件，layer播放范围修正，仅限该子元素的aeLayer

--- src/subMotion.js
+++ src/subMotion.js
@@ -1,7 +1,7 @@
 // SLeasy3-subMotion
 ;(function (SLeasy, $, T) {
     var $config = SLeasy.config(),
-        $scope  = SLeasy.scope();
+        $scope = SLeasy.scope();
 
 
     //subMotion,参数:为单个slider/detail配置对象数据
@@ -13,7 +13,7 @@
         var subName = {
             "sliders": "subMotion",
             "details": "detailMotion",
-            'floats' : 'floatElement',
+            'floats': 'floatElement',
         }
 
 
@@ -27,7 +27,7 @@
             "details": function () {
                 $config.on['detailMotion']($scope.detailIndex)
             },//详情页子元素动画开始回调
-            'floats' : ''
+            'floats': ''
         }
 
         //console.log(subMotionArr);
@@ -37,7 +37,7 @@
         //根据不同类型（幻灯或详情页），初始化timeLine及设置子动画开始、完成状态
         if (type && type != 'sliders') {
             var tl = new TimelineMax({autoRemoveChildren: $config.autoRemoveChildren, paused: true});
-            $scope[type+'TimeLine']=tl;
+            $scope[type + 'TimeLine'] = tl;
             $scope.isDetailMotion = 0;//详情页子动画开始、完成状态
         } else {
             var tl = $scope.timeLine;
@@ -47,45 +47,51 @@
 
         var totalTime = 0, startTime = 0;
         for (var i = 0; i < count; i++) {
-            var subMotion    = subMotionArr[i],//当前子动画
+            var subMotion = subMotionArr[i],//当前子动画
                 preSubMotion = subMotionArr[i - 1],//上一子动画
-                $dom         = $('#SLeasy_' + (subName[type] || type) + '_' + subMotion.index),//当前子动画元素dom
-                time         = subMotion.time || 0,//time
-                preTime      = preSubMotion && preSubMotion.time ? preSubMotion.time : 0,
+                $dom = $('#SLeasy_' + (subName[type] || type) + '_' + subMotion.index),//当前子动画元素dom
+                time = subMotion.time || 0,//time
+                preTime = preSubMotion && preSubMotion.time ? preSubMotion.time : 0,
                 /*
                  如果是第一个子动画，则当前子动画总时间累加值为，当前子动画时间，
                  如果不是第一个子动画，则当前子动画总时间累加值为，当前子动画时间:
-                    如果当前子动画有设置start值:
-                        如果preTime - subMotion.start > subMotion.time，累加0
-                        否则累加subMotion.time - (preTime - subMotion.start)
-                    如果当前子动画没有设置start值，则累加上一子动画的运动时间
+                 如果当前子动画有设置start值:
+                 如果preTime - subMotion.start > subMotion.time，累加0
+                 否则累加subMotion.time - (preTime - subMotion.start)
+                 如果当前子动画没有设置start值，则累加上一子动画的运动时间
                  */
-                totalTime    = preSubMotion ? (totalTime + (subMotion.start ? (preTime - subMotion.start > subMotion.time ? 0 : subMotion.time - (preTime - subMotion.start)) : subMotion.time)) : subMotion.time,
+                totalTime = preSubMotion ? (totalTime + (subMotion.start ? (preTime - subMotion.start > subMotion.time ? 0 : subMotion.time - (preTime - subMotion.start)) : subMotion.time)) : subMotion.time,
                 /*
                  如果是第一个子动画，则子动画开始时间为幻灯页运动完成的时间，
                  如果不是第一个子动画，则之前累加的startTime，加上当前的start值:
-                    如果当前子动画没有设置start值，则累加上一子动画的运动时间，以连接其后
-                    如果当前子动画没有设置运动时间time，则直接加0
+                 如果当前子动画没有设置start值，则累加上一子动画的运动时间，以连接其后
+                 如果当前子动画没有设置运动时间time，则直接加0
                  */
-                startTime    = preSubMotion ? (startTime + (time ? (typeof subMotion.start != 'undefined' ? subMotion.start : preTime) : 0)) : $config.motionTime,
-                subIn        = $.extend({force3D: true}, subMotion.in || {}),//in
-                subShow      = $.extend({display: 'block', force3D: true}, subMotion.show || {}),//show
-                set          = subMotion.set ? $.extend({position: 'absolute'}, subMotion.set) : {position: 'absolute'};//set
+                startTime = preSubMotion ? (startTime + (time ? (typeof subMotion.start != 'undefined' ? subMotion.start : preTime) : 0)) : $config.motionTime,
+                subIn = $.extend({force3D: true}, subMotion.in || {}),//in
+                subShow = $.extend({display: 'block', force3D: true}, subMotion.show || {}),//show
+                set = subMotion.set ? $.extend({position: 'absolute'}, subMotion.set) : {position: 'absolute'};//set
 
             //判断当前幻灯是否包含ae渲染层
             if ($dom.find('.SLeasy_ae').length) {
                 //如果渲染层所属的sliderIndex等于当前幻灯索引,则在子元素动画开始时播放ae渲染层时间线
                 $.extend(subShow, {
-                    onStart: function () {
-                        $.each($scope.aeLayer, function (index, aeLayer) {
-                            console.log(aeLayer.name);
-                            if (aeLayer.sliderIndex == $scope.sliderIndex) {
-                                aeLayer.frame = 0;//重置帧时间线
-                                console.log(aeLayer)
-                                aeLayer.autoPlay && T.to(aeLayer, aeLayer.time, aeLayer.tweenData);
-                            }
-                        });
-                    }
+                    onStart: (function (_$dom,_subMotion) {
+                        return function () {
+                            console.log(_$dom);
+                            console.log(_subMotion);
+                            $.each(_subMotion.ae.layer, function (index, layer) {
+                                console.log(layer);
+                                var layerName = layer[0];
+                                var aeLayer = $scope.aeLayer[layerName];
+                                if (aeLayer.sliderIndex == $scope.sliderIndex) {
+                                    aeLayer.frame = 0;//重置帧时间线
+                                    console.log(aeLayer)
+                                    aeLayer.autoPlay && T.to(aeLayer, aeLayer.time, aeLayer.tweenData);
+                                }
+                            });
+                        }
+                    })($dom,subMotion)
                 })
             }
 
@@ -113,11 +119,11 @@
             if (subMotion.to) {
                 for (
                     var j = 0; j < subMotion.to.length; j++) {
-                    var to         = $.extend({force3D: true}, subMotion.to[j]),
-                        preTo      = subMotion.to[j - 1] || {},
-                        time       = to.time || 0.4,
+                    var to = $.extend({force3D: true}, subMotion.to[j]),
+                        preTo = subMotion.to[j - 1] || {},
+                        time = to.time || 0.4,
                         offsetTime = preTo && (preTo.time - to.start) || 0//和上个子动画之间的间隔时间
-                        ;
+                    ;
 
                     var dom = $(SLeasy.label(to.el));
                     //console.log('===========================');
