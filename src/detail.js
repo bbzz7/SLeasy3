dba73c1d74d97d749b8b56a9cc5bd69de0e0e572
bbzz7 <bbzz7@163.com>
2017-07-15 02:26:06 +0800

    排版格式化

--- src/detail.js
+++ src/detail.js
@@ -1,7 +1,7 @@
 // SLeasy3-detail
 ;(function (SLeasy, $, T) {
     var $config = SLeasy.config(),
-        $scope  = SLeasy.scope();
+        $scope = SLeasy.scope();
 
 
     //goDetail
@@ -19,12 +19,12 @@
     }
 
     SLeasy.detailFX = function (index) {
-        var detail   = $config.details[index] || (alert('详情页索引参数错误~！')),
+        var detail = $config.details[index] || (alert('详情页索引参数错误~！')),
             motionFX = detail.motionFX || null,
             motionFX = motionFX ? SLeasy.getMotionFX(motionFX[0], motionFX[1]) : SLeasy.getMotionFX('leftRight', 0),
-            _in      = $.extend(motionFX.in, {display: 'block'}),
-            _show    = $.extend(motionFX.show, {
-                onStart   : function (e) {
+            _in = $.extend(motionFX.in, {display: 'block'}),
+            _show = $.extend(motionFX.show, {
+                onStart: function (e) {
                     detail.scroll ? SLeasy.touchScroll(true, false) : SLeasy.touchScroll(false, false);//禁止触摸默认滚动+禁止slider滑动手势
                     detail.onStart && detail.onStart();
                     SLeasy.subMotion(detail.subMotion, 'details');
@@ -34,13 +34,13 @@
                     detail.onComplete && detail.onComplete();
                 }
             }),
-            _set     = $.extend({zIndex: 1}, detail.set) || {};
+            _set = $.extend({zIndex: 1}, detail.set) || {};
 
 
         return {
-            in  : _in,
+            in: _in,
             show: _show,
-            set : _set
+            set: _set
         }
 
     }
@@ -54,10 +54,10 @@
         $scope.detailIndex = index;
 
         var detail = $config.details[index],
-            dom    = $scope.details.eq(index),
-            FX     = SLeasy.detailFX(index),
-            time   = detail.time || $config.motionTime
-            ;
+            dom = $scope.details.eq(index),
+            FX = SLeasy.detailFX(index),
+            time = detail.time || $config.motionTime
+        ;
 
         //详情页打开回调
         $config.on['detailOpen'](index);
@@ -82,24 +82,24 @@
 
 
     //closeDetail
-    SLeasy.closeDetail = function (index,callback) {
+    SLeasy.closeDetail = function (index, callback) {
         var nextIndex = SLeasy.nextDetailIndex(index);
         if ($config.routerMode) {
             var sliderHash = $scope.router.getRoute(1)
             $scope.router.setRoute(1, 'html');//设置路由
         } else {
-            SLeasy.closeDetailTransit(nextIndex,callback);
+            SLeasy.closeDetailTransit(nextIndex, callback);
         }
     }
 
-    SLeasy.closeDetailTransit = function (index,callback) {
+    SLeasy.closeDetailTransit = function (index, callback) {
         //如果详情页处于打开状态未关闭，则return
         if (!$scope.isDetail) return;
         //索引边界检查
         if (typeof index == 'undefined' || index < 0 || index > $config.details.length - 1) return;
 
-        var detail     = $config.details[index],
-            dom        = $scope.details.eq(index),
+        var detail = $config.details[index],
+            dom = $scope.details.eq(index),
             onComplete = {
                 onComplete: function () {
                     console.log(dom);
@@ -113,9 +113,9 @@
                     $config.positionMode == 'relative' && $scope.sliderBox.css("overflow", "visible");
                 }
             },
-            FX         = SLeasy.detailFX(index),
-            time       = detail.time || $config.motionTime
-            ;
+            FX = SLeasy.detailFX(index),
+            time = detail.time || $config.motionTime
+        ;
 
         //详情页关闭回调
         $config.on['detailClose'](index);
