11ec7ff0bd662c43f1061012acdcc6703bb77c51
zhuangYu <bbzz7@163.com>
2020-08-11 01:38:54 +0800

    更新自定义loading出现动画比loaded事件慢的问题

--- src/init.js
+++ src/init.js
@@ -53,14 +53,14 @@
             SLeasy.boot(dfd);
             if (!$.isEmptyObject($config.loading) && !$scope.initReady) {
                 $(".SLeasy_loading").fadeIn(300, function () {
-                    $config.loading.onComplete && $config.loading.onComplete()
-                });
-                SLeasy.subMotion($config.loading.subMotion, 'loadingElement', 0);
-                $config.loading.onStartLoad && $config.loading.onStartLoad();
-                SLeasy.init($config).done(function () {
-                    dfd.resolve();//如果有loading，第二次init完毕时，调用第一次done回调
-                    console.log('loadingReady::>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-                    $config.loading.onLoaded && $config.loading.onLoaded();//预加载完毕自定义loading回调
+                    $config.loading.onComplete && $config.loading.onComplete();
+                    SLeasy.subMotion($config.loading.subMotion, 'loadingElement', 0);
+                    $config.loading.onStartLoad && $config.loading.onStartLoad();
+                    SLeasy.init($config).done(function () {
+                        dfd.resolve();//如果有loading，第二次init完毕时，调用第一次done回调
+                        console.log('loadingReady::>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+                        $config.loading.onLoaded && $config.loading.onLoaded();//预加载完毕自定义loading回调
+                    });
                 });
             }
         });
