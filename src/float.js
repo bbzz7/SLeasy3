8a01d64bb22f75584196b2bd1fb0e22755d28333
zhuangYu <bbzz7@163.com>
2023-11-26 00:44:08 +0800

    更新floatZIndex参数

--- src/float.js
+++ src/float.js
@@ -1,7 +1,7 @@
 // SLeasy3-float
 ;(function (SLeasy, $, T) {
     var $config = SLeasy.config(),//全局配置
-        $scope  = SLeasy.scope();//公有变量
+        $scope = SLeasy.scope();//公有变量
 
     //
     SLeasy.float = function () {
@@ -9,7 +9,7 @@
         var html = SLeasy.subElement($config.floats, 'floats');
         $(html).appendTo($scope.sliderBox);
         $('.SLeasy_floatElement').each(function (index, element) {
-            T.set($(this), $.extend({zIndex: 10}, $config.floats[index].set));
+            T.set($(this), $.extend({zIndex: $config.floatZIndex || 10}, $config.floats[index].set));
         });
     }
 
