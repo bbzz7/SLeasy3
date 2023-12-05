1eb876b6000051b04d047c64ef69983398ccc41a
zhuangYu <bbzz7@163.com>
2023-12-05 17:55:19 +0800

    3.9.20(2023-12-05):更新gulpfile生产部署deploy、deploy-noMin任务;整合添加vconsole;

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-// var SLeasyPath = '../@GitHub/SLeasy3/',
-var SLeasyPath = './',
+var SLeasyPath = '../@GitHub/SLeasy3/',
     LocalPath = './',
     gulp = require('gulp'),
     $ = {},
@@ -11,6 +10,9 @@ $.concat = require(SLeasyPath + 'node_modules/gulp-concat');
 $.uglify = require(SLeasyPath + 'node_modules/gulp-uglify');
 $.replace = require(SLeasyPath + 'node_modules/gulp-replace');
 $.tinypng = require(SLeasyPath + 'node_modules/gulp-tinypng');
+$.useref = require(SLeasyPath + 'node_modules/gulp-useref');
+$.if = require(SLeasyPath + 'node_modules/gulp-if');
+$.rename = require(SLeasyPath + 'node_modules/gulp-rename');
 
 
 gulp.task('build', function () {
@@ -81,22 +83,23 @@ gulp.task('SLeasy-publish', function () {
 });
 
 gulp.task('clean', function (done) {
-    return $.del([LocalPath + '@publish/index.html']);
+    return $.del([LocalPath + '@publish/*.html']);
 });
 
 gulp.task('replace', function () {
+    var t = (new Date).getTime();
     var origin = /<!--SLeasy3-->\n(.*\n)*<!--SLeasy3 end-->/,
-        replace = '<script src="' + cdn + 'SLeasy3/build/SLeasy3.min.js?' + ((new Date).getTime()) + '" charset="utf-8"></script>';
+        replace = '<script src="' + cdn + 'SLeasy3/build/SLeasy3.min.js?' + t + '" charset="utf-8"></script>';
 
     var oldTime = /<!--timeStamp-->/,
-        newTime = '<script>window.SLeasyTimeStamp=' + (new Date).getTime() + '</script>';
+        newTime = '<script>window.SLeasyTimeStamp=' + t + '</script>';
 
     return gulp.src(LocalPath + '*.html')
         .pipe($.replace(oldTime, newTime))
         .pipe($.replace(origin, replace))
         .pipe($.replace(SLeasyPath, cdn + 'SLeasy3/'))
-        .pipe($.replace('app.js', 'app.js?' + (new Date).getTime()))
-        .pipe(gulp.dest(LocalPath + '@publish/'))
+        .pipe($.replace('app.js', 'app.js?' + t))
+        .pipe(gulp.dest(LocalPath + '@publish/'));
 });
 
 gulp.task('img-publish', function () {
@@ -111,34 +114,43 @@ gulp.task('font-publish', function () {
         .pipe(gulp.dest(LocalPath + '@publish/font/'));
 });
 
-gulp.task('js-min', function (done) {
+//js压缩任务组 ---------------------------------
+var jsMinTasks = [];
+creteJsMinTasks();
+
+function creteJsMinTasks() {
     var host = LocalPath + 'js/',
         dest = LocalPath + '@publish/js/',
         files = [
-            'app'
+            'app',
         ];
     for (var i = 0; i < files.length; i++) {
-        gulp.src([
-            LocalPath + 'js/copyRight.js',
-            host + files[i] + '.js'
-        ])
-            .pipe($.uglify({
-                output: {
-                    comments: function (n, c) {
-                        if (c.value.indexOf('庄宇') != -1) return true
+        (function (j) {
+            gulp.task('js-min' + j, function (done) {
+                gulp.src([
+                    LocalPath + 'js/copyRight.js',
+                    host + files[j] + '.js'
+                ]).pipe($.uglify({
+                    output: {
+                        comments: function (n, c) {
+                            if (c.value.indexOf('庄宇') != -1) return true
+                        }
                     }
-                }
-            }))
-            .pipe($.concat(files[i] + '.js'))
-            .pipe(gulp.dest(dest))
+                })).pipe($.concat(files[j] + '.js')).pipe(gulp.dest(dest));
+                done();
+            })
+            jsMinTasks.push('js-min' + j);
+        })(i)
     }
-    ;
-    done();
-});
+}
+
+gulp.task('js-min', gulp.series(...jsMinTasks));
+// -----------------------------------------------
 
 gulp.task('js-publish', function () {
-    return gulp.src([LocalPath + 'js/*'])
-        .pipe(gulp.dest(LocalPath + '@publish/js/'));
+    return gulp.src(['js/*', 'js/*/*', 'js/*/*/*', 'js/*/*/*/*', 'js/*/*/*/*/*'].map(function (item) {
+        return LocalPath + item;
+    })).pipe(gulp.dest(LocalPath + '@publish/js/'))
 });
 
 gulp.task('tinypng', function () {
@@ -152,6 +164,61 @@ gulp.task('endReport', function (done) {
     done();
 });
 
-gulp.task('js-build', gulp.series('js-min', 'js-publish'));
-//gulp.task('js-build', gulp.series('js-publish'));
-gulp.task('publish', gulp.series('clean', 'replace', 'img-publish', 'font-publish', 'build', 'SLeasy-publish', 'js-build', 'endReport'));
+// gulp.task('js-build', gulp.series('js-publish', 'js-min'));
+gulp.task('js-build', gulp.series('js-publish'));
+gulp.task('publish', gulp.series('clean', 'img-publish', 'font-publish', 'build', 'SLeasy-publish', 'js-build', 'replace', 'endReport'));
+
+// ！！！生产部署
+function deploy(isMinify) {
+    return async function () {
+        //抽取、合并、压缩
+        await new Promise(function (resolve) {
+            gulp.src('*.html') // 输入源文件的路径
+                .pipe($.useref())
+                .pipe($.if('*.js', $.concat('SLeasy3.js'))) // 合并所有js文件到一个文件（可选）
+                .pipe($.if('*.js', $.uglify()))         // 压缩 JavaScript 文件（可选）
+                .pipe($.if('*.js', $.rename({suffix: '.min'}))) // 重命名文件（可选）
+                .pipe(gulp.dest(LocalPath + '@publish/')) // 输出到目标目录
+                .on('end', resolve)
+        })
+        //移动js
+        await new Promise(function (resolve) {
+            gulp.src('@publish/*.js')
+                .pipe(gulp.dest('@publish/js/'))
+                .on('end', resolve)
+        })
+        //复制js
+        await new Promise(function (resolve) {
+            if (isMinify) {
+                gulp.src(['js/copyRight.js', 'js/app.js'])
+                    .pipe($.uglify({
+                        output: {
+                            comments: function (n, c) {
+                                if (c.value.indexOf('庄宇') != -1) return true
+                            }
+                        }
+                    })).pipe($.concat('app.js'))
+                    .pipe(gulp.dest('@publish/js/'))
+                    .on('end', resolve)
+            } else {
+                gulp.src(['js/copyRight.js', 'js/app.js'])
+                    .pipe($.concat('app.js'))
+                    .pipe(gulp.dest('@publish/js/'))
+                    .on('end', resolve)
+            }
+        })
+        //复制images
+        await new Promise(function (resolve) {
+            gulp.src('images/**/*')
+                .pipe(gulp.dest('@publish/images/'))
+                .on('end', resolve)
+        })
+        //清理@publish下的原始js文件
+        await $.del(['@publish/*.js']).then(function () {
+            console.log('--------------- 生产部署打包完成！---------------');
+        });
+    }
+}
+
+gulp.task('deploy', deploy(true));
+gulp.task('deploy-noMin', deploy(false));
